614a4e98527338c7caef1182a2474863b5c672b7
Tilak Chad <tilakchad111@gmail.com>
2021-05-05 20:51:40 +0545

    Updated the hash function

--- hash_table.h
+++ hash_table.h
@@ -4,9 +4,13 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include "stream.h"
 // It is the 3 byte content hash-table that will be used for efficient longest subsequence matching of the input bytes
 
 #define HASH_TABLE_SIZE 256 // Hash function -> The middle byte will be used to find the index of the bytes
+#define MAX_MATCHED_LENGTH 285
+
+
 
 typedef struct hash_entry
 {
@@ -23,7 +27,7 @@ typedef struct read_str
 
 // Inline typedef of the struct hash_entry
 
-int16_t hash_function(unsigned char, unsigned char, unsigned char); // Low effort hashing function for the 3 byte sequence
+int16_t hash_function(hash_entry);
 
 bool cmp_hash_entry(hash_entry, hash_entry);
 
@@ -31,10 +35,15 @@ bool cmp_hash_entry(hash_entry, hash_entry);
 
 hash_entry** init_hash_table();
 
-int insert_table(hash_entry* , hash_entry);
+int insert_table(hash_entry** , hash_entry);
 
-int delete_chain(hash_entry*, hash_entry* ); // Delete the hash entry of the chain after the provided hash_entry, yeah taking only pointer would work no need to pass the struct
+int delete_chain(hash_entry* ); // Delete the hash entry of the chain after the provided hash_entry, yeah taking only pointer would work no need to pass the struct
 
-hash_entry* search_table(hash_entry*, hash_entry); // don't think searching explicitly would be needed
+hash_entry* search_table(hash_entry**, hash_entry); // don't think searching explicitly would be needed
+
+// Read three upcoming bytes from the input stream and store it in the hash_entry and returns total bytes read from the stream 
+int16_t get_next3_bytes(stream*,hash_entry*); 
+
+void cleanup_hash(hash_entry**);
 
 #endif
