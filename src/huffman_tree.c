6d60725833e5bf4369a46868bc442aa49e0307c7
Tilak Chad <tilakchad111@gmail.com>
2021-05-19 20:38:13 +0545

    Rebalances the huffman tree if max code length exceed 7 for run length and 15 for literal code length to be sub optimal

--- src/huffman_tree.c
+++ src/huffman_tree.c
@@ -4,7 +4,7 @@
 #include "huffman_tree.h"
 int build_huffman_tree(priority_minheap *);
 
-int huffman_coding(int32_t* array, unsigned arr_size, int32_t* count, unsigned count_len)
+int huffman_coding(int32_t* array, unsigned arr_size, int32_t* count, unsigned count_len,int max_code_length)
 {
     int32_t count_non_zero = 0;
 
@@ -34,7 +34,14 @@ int huffman_coding(int32_t* array, unsigned arr_size, int32_t* count, unsigned c
     }
     
     build_huffman_tree(&minheap);
-
+    
+    
+    while(height(minheap.queue[0]) > max_code_length+1)
+    {
+	printf("\n\nTree rebalanced .... \n");
+	rebalance_huffman_tree_2D(minheap.queue);
+    }
+    
     code_length(minheap.queue[0], 0, count, count_len);
 
     cleanup_tree(minheap.queue[0]);
@@ -136,4 +143,98 @@ void cleanup_tree(node *tree)
     free(tree);
 }
 
+int height(node* tree)
+{
+    if (!tree)
+	return 0;
+
+    return (height(tree->left) > height(tree->right)) ? height(tree->left) + 1 : height(tree->right) + 1;
+}
+
+
+// This might not work .. so let's pass multidimensional array to rebalance the tree ... code below 
+/* int rebalance_huffman_tree(node* tree) */
+/* { */
+/*     if (!tree) */
+/* 	return 0; */
+
+/*     if (height(tree->right) - height (tree->left) == 2) */
+/*     { */
+/* 	// Do some rebalancing */
+/* 	// do left rotation here */
+	
+/*     } */
+
+/*     return rebalance_huffman_tree(tree->right); */
+    
+/* } */
+
+
+// 2D array might work but need to remain careful
+
+// This function balances the tree from top which isn't good for huffman code generation . So need to think of an alternative
+// This functions are not deleted for reference purpose (^.^)
+/* int rebalance_huffman_tree_2D(node** tree) */
+/* { */
+/*     if (!(*tree)) */
+/* 	return 0; */
+
+/*     if (height((*tree)->right) - height((*tree)->left) == 2) */
+/*     { */
+/* 	// here balancing would work .. it is analagous to passing a reference to pointer in C++.. Think nicely and you would get it */
+/* 	node* temp_root = *tree; */
+	
+/* 	*tree = temp_root->right; */
+/* 	temp_root->right = (*tree)->left; */
+/* 	(*tree)->left = temp_root; */
+/* 	return 0; */
+/* 	// Lets hope it would work */
+/*     } */
+/*     return rebalance_huffman_tree_2D(&(*tree)->right); // Else continue exploring right subtree since right subtree will generally have greater hieght in comparison to left subtree in huffman_tree */
+/*     // Will check it tomorrow */
+/* } */
+
+
+// Lol .. nice 
+int rebalance_huffman_tree_2D(node** tree)
+{
+    if (!(*tree))
+    {
+	// Returning -1 here instead of returning 0 would help to perform the rotation only once .. good 
+	return -1;
+	
+    }
+    else {
+	// Explore the deepest leaf first and backtrack to find the unbalanced node and perform tree rotation there
+	// It will ensure that rotation is performed only once during each call so that huffman tree remains sub optimal
+	// proceed toward the tree with greater height
+	int return_code = -1;
+	
+	if (height((*tree)->left) > height((*tree)->right))
+	    return_code = rebalance_huffman_tree_2D(&(*tree)->left);
+	else
+	    return_code = rebalance_huffman_tree_2D(&(*tree)->right);
+	if (return_code != -1)
+	    return return_code;
+	
+	if (height((*tree)->right) - height((*tree)->left) >= 2)
+	{
+	    // here balancing would work .. it is analagous to passing a reference to pointer in C++.. Think nicely and you would get it
+	    node* temp_root = *tree;
+	
+	    *tree = temp_root->right;
+	    temp_root->right = (*tree)->left;
+	    (*tree)->left = temp_root;
+	    return 0;
+	    // Lets hope it would work
+	}
+	else if (height((*tree)->left) - height((*tree)->left) >= 2)
+      	{
+	    printf("\n\nLeft balancing required...");
+	    // Currently halts if this condition arises. Will experiment first and I will determine if it needs to be implemented 
+	    assert(!"Left balancing not implemented .. ");
+	}
+    }
+    return -1;
+}
     
