e22b5280b0f74f455c7635582c79e951b67b6c6e
Tilak Chad <tilakchad111@gmail.com>
2021-05-19 20:37:11 +0545

    Updated header for rebalancing huffman tree

--- includes/huffman_tree.h
+++ includes/huffman_tree.h
@@ -9,11 +9,21 @@ typedef struct node {
     struct node* right;
 } node;
 
-int huffman_coding(int32_t*, unsigned, int32_t*, unsigned);
+int huffman_coding(int32_t*, unsigned, int32_t*, unsigned, int max_code_length_allowed);
 void code_length(node*, int32_t , int32_t*, unsigned);
 
 void print_tree(node*, char* , int32_t );
 
 // Take the first node of the huffman tree and deallocates 
-void cleanup_tree(node*); 
+void cleanup_tree(node*);
+
+// Just placed it here for reference purpose of failed attempt 
+int rebalance_huffman_tree(node*);
+
+// Rebalancing huffman tree if required
+int rebalance_huffman_tree_2D(node**);
+
+// Determines the height of the huffman tree. It returns actual height + 1
+int height(node*);
+
 #endif
