06bafb70e610d96ebec4f45636ce8e6b23c1500f
Tilak Chad <tilakchad111@gmail.com>
2021-05-20 13:44:09 +0545

    Added some comments

--- includes/lz77.h
+++ includes/lz77.h
@@ -4,6 +4,9 @@
 #include "stream.h"
 #include "hash_table.h"
 
+// It is the struct that will be used by/ or return by lz77 compression
+// algorithm.
+// distance referes to the go back distance in the output stream and length is the number of bytes that need to be copied from that position to the current output position 
 typedef struct length_distance
 {
     int32_t distance;
